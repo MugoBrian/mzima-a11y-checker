eeec6f3911ff56c8f4e1926faeb749f4b8f925e3
MugoBrian <mikanmuchiri@gmail.com>
2023-07-22 19:33:04 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mzima-a11y-checker-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'mzima-a11y-checker';
+}
